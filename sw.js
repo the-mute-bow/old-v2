cd9af12210a48e9d6bca0dfb56dec24c899498ae
The Mute Bow <68352266+the-mute-bow@users.noreply.github.com>
2022-04-10 14:51:43 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,26 +1,51 @@
-const version = '1.2.1';
-const log = false;
+// 22.04.10
 
-const files = [CACHE_FILES];
+const use_cache = true;
 
-self.addEventListener('install', event => {
-	event.waitUntil(
-		caches.open('static').then(cache => {
-			return cache
-				.addAll(files)
-				.then(() => self.skipWaiting())
-				.catch(error => console.error('Erreur de cache.', error));
-		})
-	);
+// Install event
+self.addEventListener('install', async e => {
+	console.log('Service worker installed');
+	self.skipWaiting();
 });
 
-self.addEventListener('fetch', event => {
-	let file = event.request.url.split('/');
-	file = file[file.length - 1];
-	event.respondWith(
-		caches.match(event.request).then(res => {
-			if (log) console.warn(`used ${res ? 'cache' : 'fetch'} for ${file}`);
-			return res ? res : fetch(event.request).catch(err => console.error(`fetch error for ${file}`));
-		})
-	);
+// Activate event
+self.addEventListener('activate', async e => {
+	console.log('Service worker activated');
+
+	// Delete main cache
+	caches.delete('main');
+
+	self.clients.claim();
 });
+
+// Fetch event
+self.addEventListener('fetch', e => e.respondWith(respond(e)));
+
+async function fetchAndCache(req, cache_name) {
+	// Fetch request
+	const fetch_res = await fetch(req).catch(e => {
+		console.error(req.url, e);
+		return null;
+	});
+
+	if (use_cache) {
+		console.info('Caching', req.url);
+
+		// Open cache and save a cloned result
+		const cache = await caches.open(cache_name);
+		cache.put(req, fetch_res.clone());
+	}
+
+	return fetch_res;
+}
+
+async function respond(e) {
+	// Try to get response from cache
+	const cached_res = await caches.match(e.request);
+
+	// If response is found, return it
+	if (cached_res) return cached_res;
+
+	// If request is not found, try to fetch it
+	return await fetchAndCache(e.request, 'main');
+}
